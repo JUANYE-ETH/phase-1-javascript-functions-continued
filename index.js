e8204a6d0f7ef8e81f87ec64850c2410670dfd0c
Juan Velasco <juanayevela@yahoo.com>
2022-04-06 13:26:09 -0400

    commit tree 271799eb27a163a5955caec955981c32c2cef3b4

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+};
+
+const mondayWork = function(event = 'go to the office') {
+    return `This Monday, I will ${event}.`;
+}
+console.log(mondayWork());
+
